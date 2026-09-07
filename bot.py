6a8ed053f82cb765e659e71f98e1fd2979cbb5c2
htetaungkyaw256163038-cpu <htetaungkyaw256163038@gmail.com>
2026-09-07 19:53:48 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ import dddocr
 import numpy as np
 from datetime import datetime, timedelta, timezone
 
-TOKEN = 'ဒီနေရာမှာ_သင့်ရဲ့_Bot_Token_အရှည်ကြီးကို_ထည့်ပါ'
+TOKEN =TOKEN = '8851853713:AAHoF5wvoib3F0sH6adR9wCn3buGVuOR3Ww'
+
 _TOKEN = ''
 OWNER = ""
 NAME = ""
