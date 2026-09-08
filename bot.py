f1c77db04f6526f145edae92473619f768d9a189
htetaungkyaw256163038-cpu <htetaungkyaw256163038@gmail.com>
2026-09-08 17:44:26 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -167,7 +167,7 @@ async def handle_result(message):
 
 async def main():
     await rebuild_session()
-    await asyncio.gather(web_server(), bot.polling(timeout=60, long_polling_timeout=60))
+    await asyncio.gather(web_server(), bot.polling(none_stop=True))
 
 if __name__ == '__main__':
     asyncio.run(main())
